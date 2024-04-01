20726d75f6fda4111c71bbfa19c17ea74bca7a3b
Maciej Malinowski <maciekmalinowski12@gmail.com>
2024-04-01 18:12:52 +0200

    EmpikScrap

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 import puppeteer from "puppeteer"
 
-async function ScrapEmpik(search){
+async function Scrap(search) {
     if (search != null) {
         const Puppe = await puppeteer.launch({
             headless: false,
             deafultViewport: null,
         });
         const page = await Puppe.newPage();
-        await page.goto("http://www.empik.com/szukaj/produkt?q="+ search +"&qtype=basicForm&ac=true", { waitUntil: "domcontentloaded", });
-        const bookInfo = await page.evaluate(() => {
-            const title = document.querySelector("a.seoTitle").innerText;
-            return title;
-        });
-        console.log(bookInfo);
+        let result = [];
+        result.push(EmpikSearch(search, page));
     }
-} 
-ScrapEmpik("Cos");
+}
+
+async function EmpikSearch(search, page) {
+    await page.goto("https://www.empik.com/ksiazki,31,s?q=" + search + "&qtype=basicForm&resultsPP=6", { waitUntil: "domcontentloaded", });
+    const bookInfo = await page.evaluate(() => {
+        const books = document.querySelectorAll("div.search-list-item-hover");
+        return Array.from(books).map((book) => {
+            let title = book.querySelector("a.seoTitle").innerText;
+            let author = book.querySelector("a.smartAuthor").innerText;
+            let price = book.querySelector("div[itemprop='price']").innerText;
+            let img = book.querySelector("img.lazy").getAttribute("lazy-img");
+            let link = "https://www.empik.com" + book.querySelector("a.seoTitle").getAttribute("href");
+            return {title, author, price, img, link};
+        });
+    });
+    console.log(bookInfo);
+    return bookInfo;
+}
+
+Scrap("Coś");
